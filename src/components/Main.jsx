9e3232b3b1aed380054c0542e70d8ce447038ccd
Fenroe <scrambles9494@gmail.com>
2022-05-16 04:12:03 +0300

    added heading to main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Main () {
   return (
     <main className="w-[417px] xl:w-1/2">
-      <h1>Hello from main</h1>
+      <section className="flex justify-between items-center h-14 w-full">
+        <h1 className="font-bold text-lg">Home</h1>
+      </section>
     </main>
   )
 }
