3713f36627bc62f8cefe408b24700df74c7520ab
Fenroe <scrambles9494@gmail.com>
2022-05-25 12:22:24 +0300

    added more albums

--- src/data/albumsData.js
+++ src/data/albumsData.js
@@ -10,13 +10,244 @@ export const albumsData = [
   },
   {
     id: 1,
-    albumOwner: 'Giles',
+    albumOwner: 'BobbyB',
     title: 'Album 2',
     coverImage: placeholder,
     lastUpdated: new Date(2022, 4, 18)
   },
   {
     id: 2,
+    albumOwner: 'The Rock',
+    title: 'My Album',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 23)
+  },
+  {
+    id: 3,
+    albumOwner: 'Banana13',
+    title: 'Album 1',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 4,
+    albumOwner: 'Proud Mom',
+    title: 'Album 2',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 5,
+    albumOwner: 'The Rock',
+    title: 'My Album',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 23)
+  },
+  {
+    id: 6,
+    albumOwner: 'Testers',
+    title: 'Test',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 7,
+    albumOwner: 'Testers',
+    title: 'Another Test',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 8,
+    albumOwner: 'The Rock',
+    title: 'My Album',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 23)
+  },
+  {
+    id: 9,
+    albumOwner: 'Giles',
+    title: 'Album 1',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 10,
+    albumOwner: 'Giles',
+    title: 'Album 2',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 11,
+    albumOwner: 'The Rock',
+    title: 'My Album',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 23)
+  },
+  {
+    id: 12,
+    albumOwner: 'Giles',
+    title: 'Album 1',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 13,
+    albumOwner: 'Giles',
+    title: 'Album 2',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 14,
+    albumOwner: 'The Rock',
+    title: 'My Album',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 23)
+  },
+  {
+    id: 15,
+    albumOwner: 'Giles',
+    title: 'Album 1',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 16,
+    albumOwner: 'Giles',
+    title: 'Album 2',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 17,
+    albumOwner: 'The Rock',
+    title: 'My Album',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 23)
+  },
+  {
+    id: 18,
+    albumOwner: 'Giles',
+    title: 'Album 1',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 19,
+    albumOwner: 'BobbyB',
+    title: 'Album 2',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 20,
+    albumOwner: 'The Rock',
+    title: 'My Album',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 23)
+  },
+  {
+    id: 21,
+    albumOwner: 'Banana13',
+    title: 'Album 1',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 22,
+    albumOwner: 'Proud Mom',
+    title: 'Album 2',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 23,
+    albumOwner: 'The Rock',
+    title: 'My Album',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 23)
+  },
+  {
+    id: 24,
+    albumOwner: 'Testers',
+    title: 'Test',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 25,
+    albumOwner: 'Testers',
+    title: 'Another Test',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 26,
+    albumOwner: 'The Rock',
+    title: 'My Album',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 23)
+  },
+  {
+    id: 27,
+    albumOwner: 'Giles',
+    title: 'Album 1',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 28,
+    albumOwner: 'Giles',
+    title: 'Album 2',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 29,
+    albumOwner: 'The Rock',
+    title: 'My Album',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 23)
+  },
+  {
+    id: 30,
+    albumOwner: 'Giles',
+    title: 'Album 1',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 31,
+    albumOwner: 'Giles',
+    title: 'Album 2',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 32,
+    albumOwner: 'The Rock',
+    title: 'My Album',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 23)
+  },
+  {
+    id: 33,
+    albumOwner: 'Giles',
+    title: 'Album 1',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 34,
+    albumOwner: 'Giles',
+    title: 'Album 2',
+    coverImage: placeholder,
+    lastUpdated: new Date(2022, 4, 18)
+  },
+  {
+    id: 35,
     albumOwner: 'The Rock',
     title: 'My Album',
     coverImage: placeholder,
