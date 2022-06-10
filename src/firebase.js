e69575ac9d1f6666aa676b611659d876e1c050e8
Fenroe <scrambles9494@gmail.com>
2022-06-10 20:22:29 +0300

    reentered firebase credentials

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage'
 import { v4 } from 'uuid'
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: 'AIzaSyAJ2sqwblxzNkKbquXWtwMYfBSP04HUU1s',
+  authDomain: 'snapshot-18036.firebaseapp.com',
+  projectId: 'snapshot-18036',
+  storageBucket: 'snapshot-18036.appspot.com',
+  messagingSenderId: '98634335563',
+  appId: '1:98634335563:web:84c711882e2d15a54954ce'
 }
 
 export const app = initializeApp(firebaseConfig)
