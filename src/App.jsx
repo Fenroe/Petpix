9f913b83d1dd937679c7c79a1d2c19d7423f2b03
Fenroe <scrambles9494@gmail.com>
2022-06-10 15:30:40 +0300

    see below

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div className="app">
       <AuthProvider>
-        <HashRouter basename="/">
+        <HashRouter basename="/#">
           <Routes>
             <Route path="*" element={
             <Private>
