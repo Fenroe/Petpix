73182042a5633e06209446567c9d6259c1fe57b8
Fenroe <scrambles9494@gmail.com>
2022-06-12 14:26:59 +0300

    added some visual polish to the login and signup screens

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -81,37 +81,37 @@ export const Login = () => {
   }
 
   return (
-    <div className="flex flex-col-reverse md:flex-row">
+    <div className="flex flex-col-reverse overflow-x-hidden md:flex-row">
       <div className="">
         <img className="w-[500px] md:h-full md:w-full object-cover" src={backgroundImage} />
       </div>
-      <main className="min-w-[500px] max-w-[500px] h-full min-h-screen bg-white flex flex-col justify-center">
-        <div className="flex flex-col items-center w-[90%] mx-auto">
+      <main className="min-w-[500px] max-w-[500px] h-screen min-h-screen bg-white flex flex-col justify-center">
+        <div className="flex flex-col gap-3 items-center w-[90%] mx-auto max-h-[700px]">
           <div className="flex flex-col justify-start w-full gap-3">
-            <GiTurtleShell className="text-7xl"/>
-            <h1 className="text-7xl font-bold font-serif">Welcome back</h1>
+            <GiTurtleShell className="text-5xl"/>
+            <h1 className="text-5xl font-bold font-serif text-center">Welcome back</h1>
           </div>
-          <form className="flex flex-col items-center mt-12" noValidate>
-            <div className="w-80 mt-8">
-              <button onClick={handleGoogleAuth} className="h-16 border-2 border-black rounded-full w-full text-xl bg-white hover:brightness-95">Continue with Google</button>
+          <form className="flex flex-col items-center" noValidate>
+            <div className="w-80">
+              <button onClick={handleGoogleAuth} className="transition-transform mt-4 h-14 border-2 border-black rounded-full w-full text-xl bg-white hover:scale-110 hover:brightness-95 focus:scale-110 focus:brightness-95">Continue with Google</button>
             </div>
-            <div className="mt-8">
+            <div className="mt-4">
               <span>OR</span>
             </div>
-            <div className="relative border-2 border-slate-400 mt-8 focus-within:border-blue-500 mb-3">
+            <div className="relative border-2 border-slate-400 mt-4 focus-within:border-blue-500 mb-3 transition-transform focus-within:scale-110">
               <input ref={emailRef} onBlur={validateEmail} required formNoValidate type="text" className="w-full px-3 pt-5 min-h-[64px] text-lg outline-none bg-none peer" />
               <label htmlFor="" className="ml-2 text-slate-400 absolute top-1/2 left-1 -translate-y-1/2 text-lg pointer-events-none duration-300 peer-valid:top-4 peer-valid:text-sm peer-focus:top-4 peer-focus:text-sm">Email</label>
             </div>
-            <div className="relative border-2 border-slate-400 focus-within:border-blue-500 mb-3">
+            <div className="relative border-2 border-slate-400 focus-within:border-blue-500 mb-3 focus-within:scale-110">
               <input ref={passwordRef} onBlur={validatePassword} required formNoValidate type="password" className="w-full px-3 pt-5 min-h-[64px] text-lg outline-none bg-none peer" />
               <label htmlFor="" className="ml-2 text-slate-400 absolute top-1/2 left-1 -translate-y-1/2 text-lg pointer-events-none duration-300 peer-valid:top-4 peer-valid:text-sm peer-focus:top-4 peer-focus:text-sm">Password</label>
             </div>
-            <button onClick={(e) => handleSubmit(e)} className="mt-3 h-10 w-48 px-3 rounded-full border-2 border-white text-xl font-bold text-white bg-red-500 hover:brightness-125">Log in</button>
-            <div className="text-red-400 p-2 max-w-full">
+            <button onClick={(e) => handleSubmit(e)} className="transition-transform mt-3 h-10 w-48 px-3 rounded-full border-2 border-white text-xl font-bold text-white bg-red-500 hover:brightness-125 hover:scale-110 focus:brightness-125 focus:scale-110">Log in</button>
+            <div className="text-red-400 p-2 max-w-full h-6">
               <span>{errorMessage}</span>
             </div>
           </form>
-          <span className="text-lg mt-3 mb-3">Don&apos;t have an account? <Link to={'/signup'}><span className="text-red-500 cursor-pointer">Sign up</span></Link></span>
+          <span className="text-lg">Don&apos;t have an account? <Link to={'/signup'}><span className="text-red-500 cursor-pointer">Sign up</span></Link></span>
         </div>
       </main>
     </div>
